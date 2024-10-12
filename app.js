5d700271eac31ea4291862fbadcf367fe3122728
fayis <fayfayis74@gmail.com>
2024-10-12 16:10:37 +0530

    cors issue fix

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const { default: mongoose } = require('mongoose')
 require('dotenv').config();
 
 
-app.use(cors())
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
+    allowedHeaders: ['Content-Type', 'Authorization'], // You can add more headers if needed
+    exposedHeaders: ['Content-Disposition'], // Important if you're sharing files, to expose file names
+    credentials: true // Set to true if you're using cookies or authorization headers
+  }));
 app.use(express.json())
 
 
